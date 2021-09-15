15295cc6262ffb20874db637d1f024e4d1a7e200
jeffrey <13910661166@139.com>
2021-09-16 00:21:19 +0800

    修改配置

--- src/main/java/io/zeebe/http/ZeebeHttpWorkerApplication.java
+++ src/main/java/io/zeebe/http/ZeebeHttpWorkerApplication.java
@@ -28,8 +28,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZeebeClient
 public class ZeebeHttpWorkerApplication extends SpringBootServletInitializer {
 
