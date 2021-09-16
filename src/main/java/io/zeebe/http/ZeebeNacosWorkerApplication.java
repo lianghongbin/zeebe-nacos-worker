2d174d85e70089c43f537f8baac3cf2b7d9dc273
hongbin liang <13910661166@139.com>
2021-09-16 17:39:55 +0800

    commit for running ok

---
+++ src/main/java/io/zeebe/http/ZeebeNacosWorkerApplication.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.http;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.EnableZeebeClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZeebeClient
+public class ZeebeNacosWorkerApplication {
+
+  @Resource
+  private DiscoveryClient nacosDiscoveryClient;
+
+  @Resource
+  private RestTemplate restTemplate;
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZeebeNacosWorkerApplication.class, args);
+  }
+
+  @ZeebeWorker
+  public void handleFooJob(final JobClient client, final ActivatedJob job) throws IOException, InterruptedException, ExecutionException, TimeoutException {
+
+    String serviceName = job.getCustomHeaders().get("service_name");
+    String uriKey = job.getCustomHeaders().get("uri_key");
+
+    System.out.println(job);
+
+    try {
+      System.out.println("-------------");
+      ServiceInstance instance = nacosDiscoveryClient.getInstances(serviceName).get(0);
+      System.out.println("======" + instance.toString());
+      String uri = instance.getMetadata().get(uriKey);
+
+      String url = "http://" + serviceName + "/" + uri;
+
+      //ServiceInstance serviceInstance = loadBalancerClient.choose("buscien-service-01-app");
+
+      System.out.println("url:" + url);
+
+      String result = restTemplate.getForObject("http://order-service/order/find", String.class);
+      System.out.println("result:====" + result);
+    }catch (Exception e) {
+      System.out.println("error:" + e.getMessage());
+    }
+
+    client.newCompleteCommand(job.getKey()).variables(job.getVariables()).send().join();
+  }
+}
